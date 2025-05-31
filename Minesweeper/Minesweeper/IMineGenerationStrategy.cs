19690c4e57ee06a720ba733b36a4774396045383
JBrincefield <jbrincefield@student.neumont.edu>
2025-05-30 23:29:03 -0600

    Strategy Pattern implementation
    created and interface that can be used by different mine generation strategies
    
    created a general mine generation strategy

---
+++ Minesweeper/Minesweeper/IMineGenerationStrategy.cs
@@ -0,0 +1,7 @@
+﻿namespace Minesweeper
+{
+    public interface IMineGenerationStrategy
+    {
+        bool[,] GenerateMines(int rows, int cols, int mineCount, (int, int) safeCell);
+    }
+}
--- Minesweeper/Minesweeper/MainPage.xaml.cs
+++ Minesweeper/Minesweeper/MainPage.xaml.cs
@@ -6,6 +6,8 @@ namespace Minesweeper
 {
     public partial class MainPage : ContentPage
     {
+        private IMineGenerationStrategy _mineStrategy = new RandomMineGenerationStrategy();
+
         private Label _mineCountLabel;
         private Button[,] _buttons;
         private bool[,] _mines;
@@ -305,23 +307,7 @@ namespace Minesweeper
 
         private void GenerateMines(int safeRow, int safeCol)
         {
-            Random random = new();
-            int minesPlaced = 0;
-
-            HashSet<(int, int)> excludedCells = GetSurroundingCells(safeRow, safeCol);
-            excludedCells.Add((safeRow, safeCol));
-
-            while (minesPlaced < _maxMines)
-            {
-                int row = random.Next(_rows);
-                int col = random.Next(_cols);
-
-                if (_mines[row, col] || excludedCells.Contains((row, col)))
-                    continue;
-
-                _mines[row, col] = true;
-                minesPlaced++;
-            }
+            _mines = _mineStrategy.GenerateMines(_rows, _cols, _maxMines, (safeRow, safeCol));
         }
 
         private HashSet<(int, int)> GetSurroundingCells(int row, int col)
